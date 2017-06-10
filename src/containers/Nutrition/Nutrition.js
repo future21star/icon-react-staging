88ebde881fbc5594f5473f1081b0f04f7a2b482e
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-06-10 13:22:56 +0600

    restrict page based on subscription level

--- src/containers/Nutrition/Nutrition.js
+++ src/containers/Nutrition/Nutrition.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
-import {BottomNav, MenubarTurquoise} from '../../components'
+import {BottomNav, MenubarTurquoise, JumbotronWhite} from '../../components';
+import {connect} from "react-redux";
+import {includes} from 'lodash';
+
+@connect(
+	state => ({
+		vaultAccess: state.auth.user.vaultAccess
+	}),
+	{}
+)
 
 export default class Nutrition extends Component {
 	render() {
+		const {vaultAccess} = this.props;
+
+		let accessToNutrition = includes(vaultAccess, 'nutrition');
+
 		return (
 			<div >
 				<Helmet title="Nutrition"/>
@@ -11,11 +24,31 @@ export default class Nutrition extends Component {
 				<MenubarTurquoise title="Nutrition"/>
 
 				<div className="container">
-					<h1>Nutrition Page</h1>
+					{accessToNutrition ? this.renderNutrition() : this.renderNoVaultAccess()}
 				</div>
 
 				<BottomNav/>
 			</div>
 		);
 	}
+
+	renderNoVaultAccess() {
+		return (
+			<div>
+				<JumbotronWhite title="No Access"
+												description={<span>You do not have access to view nutrition.</span>}
+												logo={true}/>
+			</div>
+		);
+	}
+
+	renderNutrition() {
+		return (
+			<div>
+				<JumbotronWhite title="You have access"
+												description={<span>You have access to view nutrition page.</span>}/>
+			</div>
+		);
+	}
 }
+
