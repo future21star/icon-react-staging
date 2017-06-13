14d5d2e53c479f9db7f4617d8ad1b052308af518
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-06-13 16:00:13 +0600

    desktop helpful links added

--- src/components/Desktop/BottomNavDesktop/BottomNavDesktop.js
+++ src/components/Desktop/BottomNavDesktop/BottomNavDesktop.js
@@ -3,6 +3,20 @@ import {Link} from "react-router";
 import './BottomNavDesktop.scss';
 
 export default class BottomNavDesktop extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			helpfulLinksActive: false
+		}
+	}
+
+	toggleHelpfulLinks = (e) => {
+		e.preventDefault();
+		this.setState({
+			helpfulLinksActive: !this.state.helpfulLinksActive
+		});
+	};
 
 	static linkItemRender(uri, iconName, currentUri) {
 		return (
@@ -18,6 +32,23 @@ export default class BottomNavDesktop extends Component {
 
 		return (
 			<div className="bottom-nav-wrapper-desktop">
+				<div className={`${this.state.helpfulLinksActive ? 'helpful-links-overlay' : ''}`}/>
+				<div className={`popover top helpful-links-desktop-wrapper ${this.state.helpfulLinksActive ? 'show' : ''}`}>
+					<div className="arrow"/>
+					<div className="popover-title">Helpful Links</div>
+					<div className="popover-content">
+						<div className="list-group">
+							<a href="/" className="list-group-item">Icon Assessment</a>
+							<a href="/" className="list-group-item">Travel WOD's</a>
+							<a href="/" className="list-group-item">FAQ's</a>
+							<a href="/" className="list-group-item">Facebook Group</a>
+							<a href="/" className="list-group-item">Athletes Worldwide</a>
+							<a href="/" className="list-group-item">Gear</a>
+							<a href="/" className="list-group-item">Events</a>
+						</div>
+					</div>
+				</div>
+
 				<div className="bottom-nav-desktop">
 					<div className="container-fluid">
 						<div className="pull-left">
@@ -30,7 +61,9 @@ export default class BottomNavDesktop extends Component {
 								{BottomNavDesktop.linkItemRender('/feed', 'icon-nav-feed', currentUri)}
 								{BottomNavDesktop.linkItemRender('/nutrition', 'icon-nav-nutrition', currentUri)}
 								{BottomNavDesktop.linkItemRender('/programming', 'icon-nav-programming', currentUri)}
-								{BottomNavDesktop.linkItemRender('/programming/list-view', 'icon-information', currentUri)}
+								{/*{BottomNavDesktop.linkItemRender('/programming/list-view', 'icon-information', currentUri)}*/}
+								<li><a href="#" onClick={this.toggleHelpfulLinks} className="helpful-links-item"><span
+									className="icon-information"/></a></li>
 							</ul>
 						</div>
 					</div>
