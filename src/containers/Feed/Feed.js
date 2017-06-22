432dd7d6587ed02ffadef721cb0694dd6766b5c1
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-06-22 12:23:18 +0600

    Feed started

--- src/containers/Feed/Feed.js
+++ src/containers/Feed/Feed.js
@@ -28,14 +28,12 @@ export default class Feed extends Component {
 				transitionLeave={true}
 				transitionLeaveTimeout={500}
 			>
-				<div >
+				<div className="feed-page-wrapper bottom-padding">
 					<Helmet title="Feed"/>
 
 					<MenubarTurquoise title="Feed"/>
 
-					<div className="container">
-						{accessToFeed ? this.renderFeed() : this.renderNoVaultAccess()}
-					</div>
+					{accessToFeed ? this.renderFeed() : this.renderNoVaultAccess()}
 				</div>
 			</ReactCSSTransitionGroup>
 		);
@@ -43,7 +41,7 @@ export default class Feed extends Component {
 
 	renderNoVaultAccess() {
 		return (
-			<div>
+			<div className="container">
 				<JumbotronWhite title="No Access"
 												description={<span>You do not have access to view feeds.</span>}
 												logo={true}/>
@@ -52,10 +50,126 @@ export default class Feed extends Component {
 	}
 
 	renderFeed() {
+		let imageUrl = "http://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/lowering_blood_pressure_exercise_slideshow/getty_rf_photo_of_men_lifting_weights_in_gym.jpg";
+
 		return (
 			<div>
-				<JumbotronWhite title="You have access"
-												description={<span>You have access to view feeds page.</span>}/>
+				<ul className="nav nav-pills nav-justified feed-top-sub-nav">
+					<li className="active"><a href="#"><span className="icon-feed-video"/></a></li>
+					<li><a href="#"><span className="icon-feed-podcast"/></a></li>
+					<li><a href="#"><span className="icon-feed-rehab"/></a></li>
+					<li><a href="#"><span className="icon-user-mentality"/></a></li>
+				</ul>
+
+				<div className="feed-featured-post">
+					<div className="video-wrapper">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/uiAywPQmrlM" frameBorder="0"
+										allowFullScreen/>
+					</div>
+
+					<div className="container">
+						<div className="feed-featured-post-title">Here goes to title of this post</div>
+						<div className="feed-featured-post-date">Posted 24.02.2017</div>
+						<div className="feed-featured-post-content">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab exercitationem facilis id natus nihil.
+							Consequatur eveniet expedita id in, iusto nam nobis officia porro quae quas qui saepe temporibus
+							voluptates?
+						</div>
+						<div className="feed-featured-post-read-more">
+							<a className="btn-read-more" href="#">Read more</a>
+						</div>
+					</div>
+				</div>
+
+				<div className="feed-post">
+					<div className="container">
+						<div className="row">
+							<div className="col-xs-6">
+								<img width="100%" src={imageUrl}/>
+							</div>
+							<div className="col-xs-6">
+								<div className="feed-post-title">
+									<a href="#">Demo Post Title</a>
+								</div>
+								<div className="feed-post-content">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque expedita maiores nam nesciunt
+									odio, repudiandae saepe.
+								</div>
+								<div className="feed-post-date">Posted 24.02.2017</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="feed-post">
+					<div className="container">
+						<div className="row">
+							<div className="col-xs-6">
+								<img width="100%" src={imageUrl}/>
+							</div>
+							<div className="col-xs-6">
+								<div className="feed-post-title">
+									<a href="#">Demo Post Title</a>
+								</div>
+								<div className="feed-post-content">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque expedita maiores nam nesciunt
+									odio, repudiandae saepe.
+								</div>
+								<div className="feed-post-date">Posted 24.02.2017</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="feed-post">
+					<div className="container">
+						<div className="row">
+							<div className="col-xs-6">
+								<img width="100%" src={imageUrl}/>
+							</div>
+							<div className="col-xs-6">
+								<div className="feed-post-title">
+									<a href="#">Demo Post Title</a>
+								</div>
+								<div className="feed-post-content">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque expedita maiores nam nesciunt
+									odio, repudiandae saepe.
+								</div>
+								<div className="feed-post-date">Posted 24.02.2017</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="feed-post">
+					<div className="container">
+						<div className="row">
+							<div className="col-xs-6">
+								<img width="100%" src={imageUrl}/>
+
+								<div className="feed-see-all-video">
+									<a href="#" className="btn-see-all-video">See all videos</a>
+								</div>
+							</div>
+							<div className="col-xs-6">
+								<div className="feed-post-title">
+									<a href="#">Demo Post Title</a>
+								</div>
+								<div className="feed-post-content">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque expedita maiores nam nesciunt
+									odio, repudiandae saepe.
+								</div>
+								<div className="feed-post-date">Posted 24.02.2017</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="filter-feed">
+					<a href="#" className="btn-filter-feed">
+						<span className="icon-filter"/>
+					</a>
+				</div>
 			</div>
 		);
 	}
