1539cc12b53e54afcb381a85f2f0e9ab10e0ed29
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-08-05 21:02:32 +0600

    gender based answer options done

--- src/components/assessment/StepBackSquat.js
+++ src/components/assessment/StepBackSquat.js
@@ -5,6 +5,7 @@ import {setAnswer} from "../../redux/modules/assessmentStore";
 
 @connect(
 	state => ({
+		gender: state.assessmentStore.answers[0],
 		answer: state.assessmentStore.answers[1]
 	}),
 	{setAnswer}
@@ -12,29 +13,44 @@ import {setAnswer} from "../../redux/modules/assessmentStore";
 
 export default class StepBackSquat extends Component {
 
-	answerOptions = [
-		{value: '', label: '-'},
-		{value: '1', label: '145 lbs'},
-		{value: '2', label: '185 lbs'},
-		{value: '3', label: '225 lbs'},
-		{value: '4', label: '265 lbs'},
-		{value: '5', label: '305 lbs'},
-		{value: '6', label: '345 lbs'},
-		{value: '7', label: '385 lbs'},
-		{value: '8', label: '425 lbs'},
-		{value: '9', label: '465 lbs'},
-		{value: '10', label: '>500 lbs'}
-	];
+	answerOptions = {
+		male: [
+			{value: '', label: '-'},
+			{value: '1', label: '145 lbs'},
+			{value: '2', label: '185 lbs'},
+			{value: '3', label: '225 lbs'},
+			{value: '4', label: '265 lbs'},
+			{value: '5', label: '305 lbs'},
+			{value: '6', label: '345 lbs'},
+			{value: '7', label: '385 lbs'},
+			{value: '8', label: '425 lbs'},
+			{value: '9', label: '465 lbs'},
+			{value: '10', label: '>500 lbs'}
+		],
+		female: [
+			{value: '', label: '-'},
+			{value: '1', label: '95 lbs'},
+			{value: '2', label: '115 lbs'},
+			{value: '3', label: '140 lbs'},
+			{value: '4', label: '165 lbs'},
+			{value: '5', label: '185 lbs'},
+			{value: '6', label: '210 lbs'},
+			{value: '7', label: '235 lbs'},
+			{value: '8', label: '255 lbs'},
+			{value: '9', label: '280 lbs'},
+			{value: '10', label: '>315 lbs'}
+		]
+	};
 
 	render() {
-		const {answer, setAnswer} = this.props;
+		const {answer, gender, setAnswer} = this.props;
 
 		return (
 			<Step stepTitle="Back Squat">
 				<p>Please select the weight you got closest to, if you are inbetween please select the lower option.</p>
 				<form>
 					<select className="form-control" onChange={e => setAnswer(e.target.value)} value={answer}>
-						{this.answerOptions.map((item, i) => {
+						{this.answerOptions[gender].map((item, i) => {
 							return <option value={item.value} key={i}>{item.label}</option>
 						})}
 					</select>
--- src/components/assessment/StepSnatch.js
+++ src/components/assessment/StepSnatch.js
@@ -5,6 +5,7 @@ import {setAnswer} from "../../redux/modules/assessmentStore";
 
 @connect(
 	state => ({
+		gender: state.assessmentStore.answers[0],
 		answer: state.assessmentStore.answers[6]
 	}),
 	{setAnswer}
@@ -12,28 +13,44 @@ import {setAnswer} from "../../redux/modules/assessmentStore";
 
 export default class StepSnatch extends Component {
 
-	answerOptions = [
-		{value: '', label: '-'},
-		{value: '1', label: '95 lbs'},
-		{value: '2', label: '115 lbs'},
-		{value: '3', label: '140 lbs'},
-		{value: '4', label: '165 lbs'},
-		{value: '5', label: '185 lbs'},
-		{value: '6', label: '210 lbs'},
-		{value: '7', label: '235 lbs'},
-		{value: '8', label: '255 lbs'},
-		{value: '9', label: '280 lbs'},
-		{value: '10', label: '300 lbs'}
-	];
+
+	answerOptions = {
+		male: [
+			{value: '', label: '-'},
+			{value: '1', label: '95 lbs'},
+			{value: '2', label: '115 lbs'},
+			{value: '3', label: '140 lbs'},
+			{value: '4', label: '165 lbs'},
+			{value: '5', label: '185 lbs'},
+			{value: '6', label: '210 lbs'},
+			{value: '7', label: '235 lbs'},
+			{value: '8', label: '255 lbs'},
+			{value: '9', label: '280 lbs'},
+			{value: '10', label: '300 lbs'}
+		],
+		female: [
+			{value: '', label: '-'},
+			{value: '1', label: '65 lbs'},
+			{value: '2', label: '80 lbs'},
+			{value: '3', label: '95 lbs'},
+			{value: '4', label: '110 lbs'},
+			{value: '5', label: '125 lbs'},
+			{value: '6', label: '140 lbs'},
+			{value: '7', label: '155 lbs'},
+			{value: '8', label: '170 lbs'},
+			{value: '9', label: '185 lbs'},
+			{value: '10', label: '200 lbs'}
+		]
+	};
 
 	render() {
-		const {answer, setAnswer} = this.props;
+		const {answer, gender, setAnswer} = this.props;
 
 		return (
 			<Step stepTitle="Snatch">
 				<form>
 					<select className="form-control" onChange={e => setAnswer(e.target.value)} value={answer}>
-						{this.answerOptions.map((item, i) => {
+						{this.answerOptions[gender].map((item, i) => {
 							return <option value={item.value} key={i}>{item.label}</option>
 						})}
 					</select>
