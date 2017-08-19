c68a3d9b16c820b695fbd21abfd6bf520d25554a
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-08-19 22:14:03 +0600

    Change Track WIP

--- src/containers/Nutrition/NutritionChangeTrack.js
+++ src/containers/Nutrition/NutritionChangeTrack.js
@@ -8,16 +8,34 @@ import {Link} from 'react-router';
 
 @connect(
 	state => ({
-		vaultAccess: state.authStore.user.vaultAccess
+		user: state.authStore.user
 	}),
 	{}
 )
 
 export default class NutritionChangeTrack extends Component {
-	render() {
-		const {vaultAccess} = this.props;
 
-		let accessToNutrition = includes(vaultAccess, 'nutrition');
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			selectedTrack: 'lean'
+		}
+	}
+
+	selectTrack = (selectedTrack) => {
+		this.setState({
+			selectedTrack: selectedTrack
+		});
+	};
+
+	render() {
+		const {user} = this.props;
+
+		if(!user) return <div/>;
+
+		const {selectedTrack} = this.state;
+		const image = require('../../../static/feed-default.jpg');
 
 		return (
 			<ReactCSSTransitionGroup
@@ -29,36 +47,88 @@ export default class NutritionChangeTrack extends Component {
 				transitionLeave={true}
 				transitionLeaveTimeout={500}
 			>
-				<div>
-					<Helmet title="Nutrition"/>
+				<div className="assessment-landing-wrapper bottom-padding">
+					<Helmet title="Change Track"/>
 
-					<Menubar
-						title="Nutrition"
-						leftSideContent={<Link to="profile"><span className="icon-user-profile"/>
-							<span className="mobile-hide">Profile</span>
-						</Link>}
-						className="gradient-turquoise menu-color-white">
-					</Menubar>
+					<Menubar 
+						title="Change Track"
+						className="menu-bar-white"
+						backButton={true}
+					/>
+					<div className="container-fluid">
+						<div className="assessment-tabs-nav row">
+							
+							<div onClick={e => this.selectTrack('lean')} className={`col-xs-12 col-sm-4 col-md-4 ${selectedTrack === 'lean' ? "active" : ""}`}>
+								<a href="javascript:;">The Lean Machine</a>
+							</div>
+							<div onClick={e => this.selectTrack('perfector')} className={`col-xs-12 col-sm-4 col-md-4 ${selectedTrack === 'perfector' ? "active" : ""}`}>
+								<a href="javascript:;">The Perfector</a>
+							</div>
+							<div onClick={e => this.selectTrack('gainer')} className={`col-xs-12 col-sm-4 col-md-4 ${selectedTrack === 'gainer' ? "active" : ""}`}>
+								<a href="javascript:;">The Gainer</a>
+							</div>
 
-					<div className="nutrition-change-track-content-wrapper bottom-padding">
-						<div className="select-track">
-							<h2>Please select your track</h2>
-							<p>
-								Click the button below to get more information
-							</p>
-							<button className="btn btn-primary btn-pill btn-turquoise">Select</button>
-						</div>
-						<div className="calculate-goals">
-							<h2>Calculate your goals</h2>
-							<p>
-								Use the Nutrition Calculator to find your target goals.
-							</p>
-							<button className="btn btn-primary btn-pill btn-grey">Calculate</button>
 						</div>
 					</div>
+
+					<div className="container-fluid assessment-tabs-content">
+						<div className="row">
+							<div className="col-xs-12 col-md-8 col-md-offset-2">
+							{ selectedTrack === 'lean' && (
+								<div className="assessment-section-bg">
+									<h2>The Lean Machine</h2>
+									<img src={image} width="100%"/>
+									<div>
+										Lean: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										
+										<br/>
+										<br/>
+										<button className="btn btn-primary">Select this track</button>
+									</div>
+
+								</div>
+							)}
+							{ selectedTrack === 'perfector' && (
+								<div className="assessment-section-bg">
+									<h2>The Perfector</h2>
+									<img src={image} width="100%"/>
+									<div>
+										Perfector: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									</div>
+								</div>
+							)}
+							{ selectedTrack === 'gainer' && (
+								<div className="assessment-section-bg">
+									<h2>The Gainer</h2>
+									<img src={image} width="100%"/>
+									<div>
+										Gainer: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									</div>
+								</div>
+							)}
+							</div>
+						</div>	
+					</div>
+
 				</div>
 			</ReactCSSTransitionGroup>
 		);
 	}
+
 }
 
