9dfc89e4c6b712c57485dd71360972b5a1f2ffdc
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-08-22 16:43:16 +0600

    Component Nutrition Track make goals and expectations links a toggle for content that overlays content, user can x out

--- src/components/nutrition/NutritionTrack.js
+++ src/components/nutrition/NutritionTrack.js
@@ -5,6 +5,21 @@ import {
 import nutritionTracks from '../../../api/nutritionTracks.json';
 
 export default class NutritionTrack extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			showContent: 'main'
+		}
+	}
+
+	changeContent = (content) => {
+		this.setState({
+			showContent: content
+		});
+	}
+
 	static propTypes = {
 		track: PropTypes.any.isRequired
 	};
@@ -23,6 +38,50 @@ export default class NutritionTrack extends Component {
 
 		let header = trackData.track_name.replace(/-/g, ' ');
 
+		const mainContent = (
+			<div>
+				<h2><span className={`icon-track-${trackData.track_name} icon`}/>{header}</h2>
+				<p>{trackData.desc}</p>
+				<ul className="inline-list">
+					<li><a href="javascript:;" onClick={e => this.changeContent('goal')}>Goals</a></li>
+					<li><a href="javascript:;" onClick={e => this.changeContent('expectation')}>Expectations</a></li>
+				</ul>
+				<button className="btn btn-lg btn-icon btn-icon-blue btn-icon-icon btn-icon-right">
+					Select Track
+					<span className="icon-nav-links"/>
+				</button>
+			</div>
+		);
+
+		const goalContent = (
+			<div>
+				<h4>
+					Goals
+					<button className="btn btn-default btn-xs pull-right" onClick={e => this.changeContent('main')}>x</button>
+				</h4>
+				<p className="nutrition-track-goals-desc">{trackData.goals_desc}</p>
+				<ul className="list-check nutrition-track-goals">
+				{trackData.goals.map((goal, i) => {
+			       return <li key={i}>{goal}</li>;
+			    })}
+				</ul>
+			</div>
+		);
+
+		const expectationContent = (
+			<div>
+				<h4>
+					Expectations
+					<button className="btn btn-default btn-xs pull-right" onClick={e => this.changeContent('main')}>x</button>
+				</h4>
+				<ul className="list-check nutrition-track-goals">
+				{trackData.expectations.map((expectation, i) => {
+			       return <li key={i}>{expectation}</li>;
+			    })}
+				</ul>
+			</div>
+		);
+
 		return(
 			<div className="nutriton-track-wrapper">	
 				<div className="nutrition-track-img-wrapper col-xs-12 col-sm-6" style={{backgroundImage:'url("../../nutrition-tracks/' + trackData.track_name + '.jpg")'}}>
@@ -34,33 +93,9 @@ export default class NutritionTrack extends Component {
 					/>
 				</div>
 				<div className="nutrition-track-header col-xs-12 col-sm-6">
-					<h2><span className={`icon-track-${trackData.track_name} icon`}/>{header}</h2>
-					<p>{trackData.desc}</p>
-					<ul className="inline-list">
-						<li><a href="#">Goals</a></li>
-						<li><a href="#">Expectations</a></li>
-					</ul>
-					<button className="btn btn-lg btn-icon btn-icon-blue btn-icon-icon btn-icon-right">
-						Select Track
-						<span className="icon-nav-links"/>
-					</button>
-				</div>
-				<div className="hidden-md hidden-lg hidden-sm hidden-xs">
-					<h4>Goals</h4>
-					<p className="nutrition-track-goals-desc">{trackData.goals_desc}</p>
-					<ul className="list-check nutrition-track-goals">
-					{trackData.goals.map((goal, i) => {
-				       return <li key={i}>{goal}</li>;
-				    })}
-					</ul>
-				</div>
-				<div className="hidden-md hidden-lg hidden-sm hidden-xs">
-					<h4>Expectations</h4>
-					<ul className="list-check nutrition-track-goals">
-					{trackData.expectations.map((expectation, i) => {
-				       return <li key={i}>{expectation}</li>;
-				    })}
-					</ul>
+					{this.state.showContent === 'main' && mainContent}
+					{this.state.showContent === 'goal' && goalContent}
+					{this.state.showContent === 'expectation' && expectationContent}
 				</div>
 				<div className="clearfix" />
 			</div>
