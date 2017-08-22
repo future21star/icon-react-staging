a67062d968a04a78194c18a22254a4ea9da111fa
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-08-22 20:26:43 +0600

    Nutrition index page shows selected tracks

--- src/components/nutrition/NutritionBanner.js
+++ src/components/nutrition/NutritionBanner.js
@@ -1,8 +1,15 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
 import strengthBG from '../../../static/strengthBG.jpg';
+import {connect} from "react-redux";
+import nutritionTracks from '../../../api/nutritionTracks.json';
 
-
+@connect(
+	state => ({
+		user: state.authStore.user
+	}),
+	{}
+)
 
 export default class NutritionBanner extends Component {
 	static propTypes = {
@@ -10,7 +17,20 @@ export default class NutritionBanner extends Component {
 	};
 
 	render() {
-		const {isLanding} = this.props;
+		const {isLanding, user} = this.props;
+
+		if(!user) return <div/>;
+
+		const {nutritionSelectedTrack} = user;
+
+		const trackData = nutritionTracks.nutrition_tracks.filter(item => {
+			return item.track_name === nutritionSelectedTrack;
+		})[0];
+
+		let header = "No Track Selected";
+		if(trackData) {
+			header = trackData.track_name.replace(/-/g, ' ');
+		}
 
 		let style = {backgroundImage: 'url(' + strengthBG + ')'};
 		let classes= isLanding ? 'nutrition-banner-wrapper col-xs-12 full-height-menu-header is-landing' : 'nutrition-banner-wrapper col-xs-12 col-sm-4';
@@ -19,8 +39,8 @@ export default class NutritionBanner extends Component {
 			<div className={classes} style={style}>
 				<div className="overlay"/>
 				<div className="nutrition-banner-title text-center">
-					<h2>The Lean Machine</h2>
-					<p>Goal: Sustainable Weight Loss</p>
+					<h2>{header}</h2>
+					{trackData && <p>Goal: {trackData.goals_desc}</p>}
 					<Link to="/nutrition/change-track" className="block btn btn-lg btn-icon btn-icon-outline">Change Track</Link>
 				</div>
 				{!isLanding && (
