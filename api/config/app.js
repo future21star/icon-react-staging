1a51ed6428f7dcd862d06bb8026fafcad5f4b428
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-09-07 21:27:57 +0600

    WP_API_URL changed to wp-staging

--- api/config/app.js
+++ api/config/app.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Icon Athlete Vault';
 export const APP_URL = 'https://vault.iconathlete.com';
-export const WP_API_URL = 'https://iconathlete.com/wp-json';
+export const WP_API_URL = 'https://wp-staging.iconathlete.com/wp-json';
