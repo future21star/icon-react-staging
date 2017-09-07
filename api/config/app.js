bf76e5875440b2c66ac93f12c23710273c85b80b
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-09-07 21:47:59 +0600

    dynamic WP_API_URL

--- api/config/app.js
+++ api/config/app.js
@@ -1,3 +1,12 @@
 export const APP_NAME = 'Icon Athlete Vault';
 export const APP_URL = 'https://vault.iconathlete.com';
-export const WP_API_URL = 'https://wp-staging.iconathlete.com/wp-json';
+
+
+// WP_API_URL
+let wpApiUrl = 'https://wp-staging.iconathlete.com/wp-json';
+
+if(process.env.NODE_ENV === 'production') {
+	wpApiUrl = 'https://iconathlete.com/wp-json';
+}
+
+export const WP_API_URL = wpApiUrl;
