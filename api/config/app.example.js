dcf9eeff6251e1f52fd797bf85e4902d8f113787
Syed Rezwanul Haque <rezahaque101@gmail.com>
2017-09-08 16:22:04 +0600

    app.example.js updated

--- api/config/app.example.js
+++ api/config/app.example.js
@@ -1,3 +1,23 @@
-export const APP_NAME = 'Icon Athlete Vault';
-export const APP_URL = 'https://vault.example.com';
-export const WP_API_URL = 'http://example.com/wp-json';
+// default
+let appUrl 		= 'https://react-staging.iconathlete.com';
+let wpApiUrl 	= 'https://wp-staging.iconathlete.com/wp-json';
+
+
+// override on production
+if(process.env.NODE_ENV === 'production') {
+	appUrl 		= 'https://vault.iconathlete.com';
+	wpApiUrl 	= 'https://iconathlete.com/wp-json';
+}
+
+
+// export
+export const APP_NAME 	= 'Icon Athlete Vault';
+export const APP_URL 	= appUrl;
+export const WP_API_URL = wpApiUrl;
+
+if(process.env.NODE_ENV !== 'production') {
+	console.log('=> -- using config --');
+	console.log('=> NODE_ENV', process.env.NODE_ENV || 'development');
+	console.log('=> APP_URL', APP_URL);
+	console.log('=> WP_API_URL', WP_API_URL);
+}
