dc52a488655bd2d9c87bf57c6fe497138ac363a2
Eli Weldon <eli@321goproject.com>
2017-10-26 13:16:09 +0200

    added all questions to sp assessment and styled sp landing

--- src/containers/SpecialtyPrograms/SpecialtyProgramsAssessment.js
+++ src/containers/SpecialtyPrograms/SpecialtyProgramsAssessment.js
@@ -97,39 +97,64 @@ export default class SpecialtyProgramsAssessment extends Component {
 
 							{selectedEvaluation === 'strength' && (
 								<div className="step-content">
-									Options for strength
+									<p>How many strict pull-ups can you complete?</p>
 									<form className="form-select">
 										<select className="form-control">
-											<option value="1">S 1</option>
-											<option value="2">S 2</option>
-											<option value="3">S 3</option>
+											<option value="1">0 strict pull-ups</option>
+											<option value="3">2 strict pull-ups</option>
+											<option value="5">5 strict pull-ups</option>
+											<option value="10">12 strict pull-ups</option>
+										</select>
+									</form>
+									<p>How many strict dips can you complete?</p>
+									<form className="form-select">
+										<select className="form-control">
+											<option value="1">0 strict dips</option>
+											<option value="3">2 strict dips</option>
+											<option value="5">5 strict dips</option>
+											<option value="10">12 strict dips</option>
 										</select>
 									</form>
 								</div>
 							)}
 							{selectedEvaluation === 'technique' && (
-									<div className="step-content">
-										Options for technique
-										<form className="form-select">
-											<select className="form-control">
-												<option value="1">T 1</option>
-												<option value="2">T 2</option>
-												<option value="3">T 3</option>
-											</select>
-										</form>
-									</div>
+								<div className="step-content">
+									<p>Please select your False Grip ability:</p>
+									<form className="form-select">
+										<select className="form-control">
+											<option value="1">Not able to use false grip on low rings.</option>
+											<option value="5">Can apply false grip low rings, not high rings.</option>
+											<option value="10">False grip pull up while hanging on high rings.</option>
+										</select>
+									</form>
+									<form className="form-select">
+										<select className="form-control">
+											<option value="1">Rings pull to the armpits instead of the sternum and athlete skips steps during transition on low rings.</option>
+											<option value="5">Transition rings to sternum low feet under rings, rings go to armpits feet extended in front of the body.</option>
+											<option value="10">Athlete can successfully perform an l-sit transition on low rings while rings pull to sternum and trace the bottom pecs.</option>
+										</select>
+									</form>
+								</div>									
 							)}
 							{selectedEvaluation === 'flexibility' && (
-									<div className="step-content">
-										Options for flexibility
-										<form className="form-select">
-											<select className="form-control">
-												<option value="1">F 1</option>
-												<option value="2">F 2</option>
-												<option value="3">F 3</option>
-											</select>
-										</form>
-									</div>
+								<div className="step-content">
+									<p>Please select your current Shoulder position when performing a ring dip:</p>
+									<form className="form-select">
+										<select className="form-control">
+											<option value="1">Shoulder remains above elbow at bottom of ring dip hold.</option>
+											<option value="5">Shoulder at parallel in bottom of dip position.</option>
+											<option value="10">Shoulder below elbow ring dip hang.</option>
+										</select>
+									</form>
+									<p>Please select the movement of the rings during transition</p>
+									<form className="form-select">
+										<select className="form-control">
+											<option value="1">Rings cannot come to sternum with feet supporting on low rings.</option>
+											<option value="5">Rings trace nipple line in transition.</option>
+											<option value="10">Rings trace under pecs through transition.</option>
+										</select>
+									</form>
+								</div>
 							)}
 
 							<div className="bottom-padding text-center">
