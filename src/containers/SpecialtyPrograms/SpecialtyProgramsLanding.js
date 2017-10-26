173861ab943fa7130c0e77a788edd67dad0bcddd
Eli Weldon <eli@321goproject.com>
2017-10-26 11:45:52 +0200

    styling specialty programs landing

--- src/containers/SpecialtyPrograms/SpecialtyProgramsLanding.js
+++ src/containers/SpecialtyPrograms/SpecialtyProgramsLanding.js
@@ -7,6 +7,7 @@ import {Link} from "react-router";
 
 @connect(
 		state => ({
+			browser: state.browser,
 			user: state.authStore.user
 		}),
 		{}
@@ -39,31 +40,30 @@ export default class SpecialtyProgramsLanding extends Component {
 						transitionLeave={true}
 						transitionLeaveTimeout={500}
 				>
-					<div className="assessment-landing-wrapper bottom-padding full-height-header menu-head-buffer" style={style}>
+					<div className="assessment-landing-wrapper bottom-padding">
 						<Helmet title="Specialty Programs"/>
 						<div className="bg-overlay"/>
 						<div className="container-fluid">
+							<div className="col-xs-12 col-md-6 text-center sp-program-heade" style={style}>
+								<h1 className="text-red">MUSCLE UP</h1>
+								<h1 className="text-white">DEVELOPMENT PROGRAM</h1>
+							</div>
 							<div className="col-xs-12 col-md-6 text-center">
-								<div className="sp-program-header">
-									<h1 className="text-red">MUSCLE UP</h1>
-									<h1 className="text-white">DEVELOPMENT PROGRAM</h1>
-									<h2>Find Your Track</h2>
-									<Link to="specialty-programs/assessment" className="btn btn-lg btn-icon">
-										Take Assessment
+								<h2>Find Your Track</h2>
+								<Link to="specialty-programs/assessment" className="btn btn-lg btn-icon">
+									Take Assessment
+								</Link>
+								<h2>Programming</h2>
+								<div className="action-button sp-program-actions">
+									<Link to="/specialty-programs/view" className="btn btn-lg btn-icon">
+										Strength
 									</Link>
-									<h2>Programming</h2>
-									<div className="action-button sp-program-actions">
-										<Link to="/specialty-programs/view" className="btn btn-lg btn-icon">
-											Strength
-										</Link>
-										<Link to="/specialty-programs/view" className="btn btn-lg btn-icon" style={{'margin-left': '5px'}}>
-											Technique
-										</Link>
-									</div>
+									<Link to="/specialty-programs/view" className="btn btn-lg btn-icon" style={{'margin-left': '5px'}}>
+										Technique
+									</Link>
 								</div>
 							</div>
 						</div>
-
 						<BottomNavAuth/>
 					</div>
 
